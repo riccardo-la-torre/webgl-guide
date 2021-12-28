863592bb8186d2dce963888c2b734caf13811c94
Riccardo La Torre <ricc.latorre@gmail.com>
2021-12-28 23:03:42 +0000

    Migrate to webpack

---
+++ index.js
@@ -0,0 +1,4 @@
+import "./lib/webgl-utils.js";
+import "./lib/webgl-debug.js";
+import "./lib/cuon-utils.js";
+import "./lib/cuon-matrix.js";
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webgl.bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.vert|\.frag$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
