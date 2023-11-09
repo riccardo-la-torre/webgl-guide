46e5913f0fa5f162df515dc722bbfc67b152a85f
Riccardo La Torre <ricc.latorre@gmail.com>
2023-11-09 15:14:37 +0000

    earth dist as docs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "webgl.bundle.js",
   },
   module: {
