72438af0fb9e30c43901cf31e271afd367752810
Riccardo La Torre <ricc.latorre@gmail.com>
2023-11-22 17:44:18 +0000

    prod build

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 const webpack = require("webpack"); //to access built-in plugins
 
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "development" : "production",
   entry: "./index.js",
   output: {
+    clean: true,
     path: path.resolve(__dirname, "docs"),
-    filename: "webgl.bundle.js",
+    filename:
+      process.env.NODE_ENV === "production"
+        ? "webgl.[contenthash].bundle.js"
+        : "webgl.bundle.js",
   },
   module: {
     rules: [
